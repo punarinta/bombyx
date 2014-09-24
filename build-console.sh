e0a50fc6b0f551f19c9d2b5fb2aef7666f9938e2
punarinta <cheaterx@yandex.ru>
2014-09-24 07:08:10 +0200

    link with 'dl'

--- build-console.sh
+++ build-console.sh
@@ -6,7 +6,7 @@ larva.c \
 console.c \
 \
 -std=c99 \
--O0 -g -o bombyx
+-O0 -g -o bombyx -ldl
 
 # valgrind --tool=memcheck --leak-check=full --show-leak-kinds=definite ./bombyx tests/1.leaf -v
 # http://www.yolinux.com/TUTORIALS/LibraryArchives-StaticAndDynamic.html
