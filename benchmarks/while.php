ca6d3f4df4af98497dd0119de5c1b8fb8a5a3ac2
punarinta <cheaterx@yandex.ru>
2014-09-29 16:37:02 +0200

    benchmarks

---
+++ benchmarks/while.php
@@ -0,0 +1,12 @@
+<?php
+
+$xy = microtime(true);
+$n = 10000;
+
+while (--$n)
+{
+    $a = 'hello, world!';
+}
+
+// multiplication and formatting do not count as they go after the subtraction
+echo ("\nExecution took: " . number_format((microtime(true) - $xy) * 1000000, 0, '.', '') . " microsecond(s).\n");
