a780bd965f295d01237b4cc666eb7023395b5c8d
punarinta <cheaterx@yandex.ru>
2014-10-01 09:10:38 +0200

    gcc flag

--- build-console.sh
+++ build-console.sh
@@ -7,7 +7,7 @@ console.c \
 -std=c99 \
 -O0 -g -o bombyx -ldl
 
-# -O3 -o bombyx -ldl
+# -O3 -DNDEBUG -o bombyx -ldl
 
 # valgrind --tool=memcheck --leak-check=full --show-leak-kinds=definite ./bombyx tests/1.leaf -v
 # valgrind --tool=callgrind --callgrind-out-file=callgrind.out ./bombyx tests/1.leaf
