1c9cb8680dbe92206b78e5069247d9665f789d7b
punarinta <cheaterx@yandex.ru>
2014-11-10 09:38:58 +0100

    web cocoon builder fix

--- cocoons/web/build.sh
+++ cocoons/web/build.sh
@@ -2,8 +2,9 @@
 
 gcc \
 web.c \
+../../core/*.c \
 \
 -std=c99 \
--O0 -shared -fPIC -o ../web.ccn
+-O0 -shared -fPIC -o ../web.ccn -L../../vendor/ -ljansson
 
 # -O3 -DNDEBUG -shared -fPIC -o ../web.ccn
