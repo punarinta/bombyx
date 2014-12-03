940809b8d1147e1e5698d351374c8d45e9fda744
punarinta <cheaterx@yandex.ru>
2014-12-03 17:00:53 +0100

    mysql::query()

--- cocoons/mysql/mysql.c
+++ cocoons/mysql/mysql.c
@@ -40,7 +40,7 @@ var connect_(bombyx_env_t *env, BYTE argc, var *stack)
 {
     if (argc < 4)
     {
-        return cocoon_error(env, "Parameters should be of type STRING.");
+        return cocoon_error(env, "Some crap with function arguments, see manual.");
     }
 
     unsigned int port = 0;
@@ -53,11 +53,32 @@ var connect_(bombyx_env_t *env, BYTE argc, var *stack)
     v.data = mysql_init(NULL);
     if (!mysql_real_connect(v.data, stack[0].data, stack[1].data, stack[2].data, stack[3].data, port, NULL, 0))
     {
-    	return cocoon_error(env, "DB error: %s\n", mysql_error(v.data));
+    	return cocoon_error(env, "MySQL connection error: %s", mysql_error(v.data));
     }
 
     v.type = VAR_CUSTOM;
     v.data_size = sizeof(MYSQL);
+
+    return v;
+}
+
+var query_(bombyx_env_t *env, BYTE argc, var *stack)
+{
+    if (argc != 2 || stack[1].type != VAR_STRING)
+    {
+        return cocoon_error(env, "Second parameter should be of type STRING.");
+    }
+
+	if (mysql_query(stack[0].data, stack[1].data))
+	{
+		return cocoon_error(env, "MySQL query error: %s", mysql_error(stack[0].data));
+	}
+
+	var v = {0};
+
+    v.type = VAR_CUSTOM;
+    v.data = mysql_use_result(stack[0].data);
+    v.data_size = sizeof(MYSQL_RES);
 
     return v;
 }
