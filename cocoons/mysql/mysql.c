468243890b43b505c22871e5988e6632e6503b9d
punarinta <cheaterx@yandex.ru>
2014-12-04 14:33:16 +0100

    mysql::row()

--- cocoons/mysql/mysql.c
+++ cocoons/mysql/mysql.c
@@ -34,7 +34,7 @@ var version_()
  * @param string database
  * @param double port
  *
- * @return string
+ * @return pointer
  */
 var connect_(bombyx_env_t *env, BYTE argc, var *stack)
 {
@@ -63,12 +63,12 @@ var connect_(bombyx_env_t *env, BYTE argc, var *stack)
 }
 
 /**
- * Connects to MySQL.
+ * Sends a query to MySQL.
  *
- * @param custom sql-connection
+ * @param pointer sql-connection
  * @param string query
  *
- * @return string
+ * @return pointer
  */
 var query_(bombyx_env_t *env, BYTE argc, var *stack)
 {
@@ -92,12 +92,11 @@ var query_(bombyx_env_t *env, BYTE argc, var *stack)
 }
 
 /**
- * Connects to MySQL.
+ * Fetches a row from MySQL result.
  *
- * @param custom sql-result
- * @param string query
+ * @param pointer sql-result
  *
- * @return string
+ * @return array
  */
 var fetchRow_(bombyx_env_t *env, BYTE argc, var *stack)
 {
@@ -134,6 +133,61 @@ var fetchRow_(bombyx_env_t *env, BYTE argc, var *stack)
 
         array_push(v.data, v2);
     }
+
+    return v;
+}
+
+/**
+ * Performs a full row fetch cycle. Use it if you're fetching 1 row only.
+ *
+ * @param pointer sql-connection
+ * @param string query
+ *
+ * @return array
+ */
+var row_(bombyx_env_t *env, BYTE argc, var *stack)
+{
+    if (argc < 2 || stack[1].type != VAR_STRING)
+    {
+        return cocoon_error(env, "Second parameter should be of type STRING.");
+    }
+
+	if (mysql_query(stack[0].data, stack[1].data))
+	{
+		return cocoon_error(env, "MySQL query error: %s", mysql_error(stack[0].data));
+	}
+
+    var v = {0};
+	MYSQL_RES *res = mysql_use_result(stack[0].data);
+	MYSQL_ROW row = mysql_fetch_row(res);
+
+	if (!row)
+    {
+        // no data was fetched
+        return v;
+    }
+
+    unsigned int num_rows = mysql_num_fields(res);
+
+    v.type = VAR_ARRAY;
+    v.data = array_create(num_rows);
+    v.data_size = sizeof(array_t);
+
+    for (unsigned int i = 0; i < num_rows; i++)
+    {
+        var v2 = {0};
+
+        if (row[i])
+        {
+            v2.type = VAR_STRING;
+            v2.data = strdup(row[i]);
+            v2.data_size = strlen(v2.data) + 1;
+        }
+
+        array_push(v.data, v2);
+    }
+
+    mysql_free_result(res);
 
     return v;
 }
