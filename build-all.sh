1452986da5d98b4f69c1ae83c5ca3339291de5a7
punarinta <cheaterx@yandex.ru>
2014-12-05 09:45:51 +0100

    build-all script

--- build-all.sh
+++ build-all.sh
@@ -2,8 +2,14 @@
 
 cd "$(dirname "$0")"
 
+echo "Building CLI version..."
 ./build-console.sh
+
+echo "Building web version..."
 ./build-web.sh
+
+echo "Building cocoons..."
 cocoons/web/build.sh
 cocoons/text/build.sh
 cocoons/json/build.sh
+cocoons/mysql/build.sh
