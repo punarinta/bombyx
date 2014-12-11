6f632c259f45250018f78b8b749539b4dc73b98d
punarinta <cheaterx@yandex.ru>
2014-12-11 19:25:50 +0100

    link with math lib

--- build-console.sh
+++ build-console.sh
@@ -7,9 +7,9 @@ core/*.c \
 console.c \
 \
 -std=c99 \
--O0 -g -o bombyx -ldl -Lvendor/ -ljansson
+-O0 -g -o bombyx -ldl -lm -Lvendor/ -ljansson
 
-# -O3 -DNDEBUG -o bombyx -ldl -Lvendor/ -ljansson
+# -O3 -DNDEBUG -o bombyx -ldl -lm -Lvendor/ -ljansson
 
 # valgrind --tool=memcheck --leak-check=full --show-leak-kinds=definite ./bombyx tests/1.leaf -v
 # valgrind --tool=callgrind --callgrind-out-file=callgrind.out ./bombyx tests/1.leaf
--- build-web.sh
+++ build-web.sh
@@ -7,6 +7,6 @@ core/*.c \
 web.c \
 \
 -std=c99 \
--D WEB_BUILD=1 -O0 -o bombyx-web -lpthread -ldl -lfcgi -ldl -Lvendor/ -ljansson
+-D WEB_BUILD=1 -O0 -o bombyx-web -lpthread -ldl -lm -lfcgi -ldl -Lvendor/ -ljansson
 
-# -D WEB_BUILD=1 -O3 -DNDEBUG -o bombyx-web -lpthread -ldl -lfcgi -ldl -Lvendor/ -ljansson
+# -D WEB_BUILD=1 -O3 -DNDEBUG -o bombyx-web -lpthread -ldl -lm -lfcgi -ldl -Lvendor/ -ljansson
