84aca9c21a49d126a10166344bfe35a1b69e7e92
punarinta <cheaterx@yandex.ru>
2014-12-12 08:46:15 +0100

    stack checks

--- core/larva_silk.c
+++ core/larva_silk.c
@@ -15,12 +15,20 @@
 
 inline void stack_push(bombyx_env_t *env, var v)
 {
+    if (env->bc_stack_size > BOMBYX_STACK_SIZE - 1)
+    {
+        larva_error(env, "Stack overflow. :(");
+    }
     v.level = env->gl_level;
     env->bc_stack[env->bc_stack_size++] = v;
 }
 
 inline var stack_pop(bombyx_env_t *env)
 {
+    if (env->bc_stack_size <= 0)
+    {
+        larva_error(env, "Stack is empty. Pop failed.");
+    }
     return env->bc_stack[--env->bc_stack_size];
 }
 
